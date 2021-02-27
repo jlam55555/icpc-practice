8b98dd96854aa8972391ba2c0cc335a39de2777a
Jonathan Lam <jlam55555@gmail.com>
2021-02-27 15:20:35 -0500

    fixed 2018f

--- 2018f/2018f_lam.cpp
+++ 2018f/2018f_lam.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <stdio.h>
 #include <cstring>
+#include <deque>
+#include <cmath>
 
 using namespace std;
 
@@ -18,42 +20,31 @@ int main()
 		// get dataset number
 		cin >> K >> n >> k >> pl >> pr;
 
-		cout << "STARTED" << endl;
-
 		// init probabilities
 		double dp[n] = {0}, dp2[n];
 		dp[0] = 1;
 		int sodp = sizeof dp;
-
-// 0 1 2 3
-// --------
-// 1 0 0 0         1        P(2 wins) = 0
-// 0 0.6 0 0.3     0.9        P(2 wins) = 0
-// 0.36 0 0.45 0   0.81       P(2 wins) = 0.45
+		deque<double> hist;
 
 		// iterate
 		double pround = 1, ratio = pr + pl, pwin = 0;
-
-
-		for (int round = 0; round <= 1000; round++) {
+		int round;
+		for (round = 0; hist.size() < 10 || (hist.front() - hist.back()) > 1e-8; round++) {
 			pround *= ratio;
 			pwin += dp[k] * (1 - ratio);
+
+			hist.push_front(pwin);
+			if (hist.size() > 10) {
+				hist.pop_back();
+			}
+
 			for (int j = 0; j < n; j++) {
-				dp2[j] = dp[(j+1)%n] * pl + dp[(j+n-1)%n] * pr;
+				dp2[j] = dp[(j+1)%n] * pr + dp[(j+n-1)%n] * pl;
 			}
 			memcpy(dp, dp2, sodp);
-			
-
-			// cout << "ROUND " << round;
-			// for (int j = 0; j < n; j++) {
-			// 	printf(" %.10f", dp[j]);
-			// }
-			// cout << " " << pwin << endl;
 		}
 
-		// print result
-		//cout << K << " " << (pwin*100 << endl;
-		printf("%d %f\n", K, pwin*100);
+		printf("%d %.2f\n", K, pwin*100);
 	}
 
 	return 0;
